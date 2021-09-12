9dfc8546b5d7dcf218b703c34a00fb3898f8fb33
Rogelio Martinez <rmartinez5926.rm@gmail.com>
2021-09-12 15:38:50 -0400

    create login page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Detail from './components/Detail';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
       <Router>
         <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/detail">
             <Detail />
           </Route>
